74d2d8cb661a5978f31aff4982a44bb72fbfd24a
Nyakrasanim <48143207+Nyakrasanim@users.noreply.github.com>
2021-12-23 19:27:08 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,11 @@
 <?php
-$ host = "host lokal" ;
-$ namapengguna = "akar" ;
-$ sandi = "" ;
-$ Databasename = "akademik" ;
-$ con = @mysqli_connect ( $ host , $ username , $ password , $ databasename );
-jika (! $ kon ) {
-    gema  "Kesalahan: " . mysqli_connect_error ();
+$host="localhost";
+$username="root";
+$password="";
+$databasename="akademik";
+$con=@mysqli_connect($host,$username,$password,$databasename);
+if (!$con) {
+    echo "Error: " . mysqli_connect_error();
+    exit();
 }
-// lain {
-// echo "berhasil";
-// }
+?>
